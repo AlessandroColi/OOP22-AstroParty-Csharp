b2284c956a8f323bd93868699ff65e6ad6595c4f
Alessandro Coli - alessandro.coli2@studio.unibo.it <alessandro.coli2@studio.unibo.it>
2023-04-18 14:40:40 +0200

    PowerUpSpawner

--- ColiAlessandro/AstroParty/game/powerup/impl/PowerUpSpawner.cs
+++ ColiAlessandro/AstroParty/game/powerup/impl/PowerUpSpawner.cs
@@ -3,11 +3,12 @@ namespace ColiAlessandro.AstroParty.game.powerup.impl
 {
     public class PowerUpSpawner : IPowerUpSpawner
     {
-        private readonly Collection<EntityType> _possiblePowerUpTypes;
+        private readonly List<EntityType> _possiblePowerUpTypes;
         private readonly long _spawnDelay;
         private IGameState _world; 
         private readonly IPowerUpFactory _pUPfactory = new PowerUpFactory();
         private Timer _timer = new System.Timers.Timer();
+        private Random _random = new Random();
 
 
         /// <param name= possiblePowerUpTypes> a collection of the possible types of PowerUPs. </param>
@@ -29,51 +30,44 @@ namespace ColiAlessandro.AstroParty.game.powerup.impl
             _timer.Stop();
         }
 
-                /**
-        * creates and adds a new powerUp to the world.
-        */
+        ///<summary>
+        /// creates and adds a new powerUp to the world.
+        ///</summary>
         private void generate() {
             //System.out.println("spawn");
             if (this.world != null && this.world.getPowerUps().size() < PowerUp.MAX_ON_SCREEN) {
                 this.world.addPowerUp(this.pUPfactory.createPowerUp(this.generateType(), this.generatePos()));
             }
         }
-    //TODO: da qua in poi sono ancora su java e va tradotta
-        /**
-        * generate the type of the new Power Up between the active ones in the world.
-        * @return the {@link PowerUpTypes}
-        */
+        
+        ///<summary>
+        // generate the type of the new Power Up between the active ones in the world.
+        ///</summary>
+        /// <return> the {@link PowerUpTypes} </return>
         private EntityType generateType() {
-
-            final int rand = random.nextInt(this.possiblePowerUpTypes.size());
-            final var it = this.possiblePowerUpTypes.iterator();
-
-            for (int i = 0; i < rand; i++) {
-                it.next();    //non controllo perche' ho preso un num minore di size quindi deve esserci qualcosa;
-            }
-
-            return it.next();
+            int rand = random.NextInt64(_possiblePowerUpTypes.Count);
+            return _possiblePowerUpTypes.ElementAt(rand);
         }
 
-        /**
-        * generates a possible {@link Position} for the new PowerUp.
-        * @return the position.
-        */
+        ///<summary>
+        // generates a possible {@link Position} for the new PowerUp.
+        ///</summary>
+        /// <return> the position </return>
         private Position generatePos() {
 
             Position pos;
 
             do {
-                pos = new Position(random.nextDouble(GameState.WIDTH - PowerUp.RELATIVE_SIZE * 2) + PowerUp.RELATIVE_SIZE,
-                        random.nextDouble(GameState.HEIGHT - PowerUp.RELATIVE_SIZE * 2) + PowerUp.RELATIVE_SIZE);
+                pos = new Position(random.nextDouble(IGameState.WIDTH - IPowerUp.RELATIVE_SIZE * 2) + IPowerUp.RELATIVE_SIZE,
+                        random.nextDouble(IGameState.HEIGHT - IPowerUp.RELATIVE_SIZE * 2) + IPowerUp.RELATIVE_SIZE);
 
             } while (canExist(pos));
 
             return pos;
         }
 
-        private boolean canExist(final Position position) {
-            final CircleHitBox hbox = new CircleHitBoxImpl(position, PowerUp.RELATIVE_SIZE);
+        private boolean canExist(Position position) {
+            final CircleHitBox hbox = new CircleHitBoxImpl(position, PowerUp.RELATIVE_SIZE); //TODO ?
             return  this.world.getEntities().stream()
                     .map(entity -> entity.getHitBox())
                     .anyMatch(e -> e.checkCollision(hbox));
