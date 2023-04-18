1650348f98c47b84186d7b79ceb7b0735c3ad745
Alessandro Coli - alessandro.coli2@studio.unibo.it <alessandro.coli2@studio.unibo.it>
2023-04-18 10:49:24 +0200

    PowerUpSpawner: impl di stop

--- ColiAlessandro/AstroParty/game/powerup/impl/PowerUpSpawner.cs
+++ ColiAlessandro/AstroParty/game/powerup/impl/PowerUpSpawner.cs
@@ -22,7 +22,61 @@ namespace ColiAlessandro.AstroParty.game.powerup.impl
             _timer.Interval = _spawnDelay;
             _timer.AutoReset = true;
             _timer.Elapsed += ( sender, e ) => Generate();
-            _timer.start();
+            _timer.Start();
         }
+
+        override vois Stop(){
+            _timer.Stop();
+        }
+
+                /**
+        * creates and adds a new powerUp to the world.
+        */
+        private void generate() {
+            //System.out.println("spawn");
+            if (this.world != null && this.world.getPowerUps().size() < PowerUp.MAX_ON_SCREEN) {
+                this.world.addPowerUp(this.pUPfactory.createPowerUp(this.generateType(), this.generatePos()));
+            }
+        }
+    //TODO: da qua in poi sono ancora su java e va tradotta
+        /**
+        * generate the type of the new Power Up between the active ones in the world.
+        * @return the {@link PowerUpTypes}
+        */
+        private EntityType generateType() {
+
+            final int rand = random.nextInt(this.possiblePowerUpTypes.size());
+            final var it = this.possiblePowerUpTypes.iterator();
+
+            for (int i = 0; i < rand; i++) {
+                it.next();    //non controllo perche' ho preso un num minore di size quindi deve esserci qualcosa;
+            }
+
+            return it.next();
+        }
+
+        /**
+        * generates a possible {@link Position} for the new PowerUp.
+        * @return the position.
+        */
+        private Position generatePos() {
+
+            Position pos;
+
+            do {
+                pos = new Position(random.nextDouble(GameState.WIDTH - PowerUp.RELATIVE_SIZE * 2) + PowerUp.RELATIVE_SIZE,
+                        random.nextDouble(GameState.HEIGHT - PowerUp.RELATIVE_SIZE * 2) + PowerUp.RELATIVE_SIZE);
+
+            } while (canExist(pos));
+
+            return pos;
+        }
+
+        private boolean canExist(final Position position) {
+            final CircleHitBox hbox = new CircleHitBoxImpl(position, PowerUp.RELATIVE_SIZE);
+            return  this.world.getEntities().stream()
+                    .map(entity -> entity.getHitBox())
+                    .anyMatch(e -> e.checkCollision(hbox));
+    }
     }
 }
