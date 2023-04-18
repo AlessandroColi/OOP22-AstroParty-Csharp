63bf5a1bfe6f5929cd64e74e1209466bf973f158
Alessandro Coli - alessandro.coli2@studio.unibo.it <alessandro.coli2@studio.unibo.it>
2023-04-18 15:25:15 +0200

    PlayerId methods

--- ColiAlessandro/AstroParty/common/PlayerId.cs
+++ ColiAlessandro/AstroParty/common/PlayerId.cs
@@ -7,4 +7,13 @@ namespace ColiAlessandro.AstroParty.common
         PLAYER3,
         PLAYER4
     }
+
+    public static class PlayerIdExtension
+    {
+        private Dictionary<PlayerId,string> _map = new Dictionary<PlayerId,string>();
+
+        string GetName( PlayerId id) => _map.TryGetValue( id, "Player " + ((int)id).ToString() );
+
+        void SetName( PlayerId id, string name) => _map.TryAd(id, name); // una volta scelto il nome non si può cambiare
+    }
 }
